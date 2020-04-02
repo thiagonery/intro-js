7a2d5eccdb8b5eb870f7dd3aa9082600e0bdcfc5
thiagonery <60719065+thiagonery@users.noreply.github.com>
2020-04-01 22:37:07 -0300

    Update Exe1.js

--- src/Exe1.js
+++ src/Exe1.js
@@ -19,7 +19,41 @@
  * @param {Number} numero2
  * @returns {Number} resultado da operação
  */
-function operacoesBasicas(operacao, numero1, numero2) { }
+function operacoesBasicas(operacao, numero1, numero2) { 
+
+numero3
+
+if (operacao == 'a') {
+    
+    numero3 = numero1 + numero2;
+
+}else if (operacao == 's'){
+
+    numero3 = numero1 - numero2
+
+}else if (operacao == 'm'){
+
+    numero3 = numero1 * numero2
+
+}else if (operacao == 'd'){
+
+    numero3 = parseFloat( numero1 / numero2 )
+
+}else if (operacao == 'p'){
+
+    for (let i = 0; i < numero2; i++){
+        numero3 *= numero1;
+    }
+
+}else {
+
+return undefied
+
+}
+
+return numero3
+
+}
 
 /**
  * Escreva uma função que recebe dois elementos e retorna uma mensagem dizendo se eles são estritamente iguais,
@@ -33,4 +67,22 @@ function operacoesBasicas(operacao, numero1, numero2) { }
  * @param {*} elemento2
  * @returns {string} mensagem com o resultado da comparação 
  */
-function comparadorBasico(elemento1, elemento2) { }
+function comparadorBasico(elemento1, elemento2) {
+
+if(elemento1 === elemento2){
+
+    mensagem = 'Elemento ' + elemento1 + ' (' + (typeof elemento1) + ')' +' é estritamente igual ao elemento ' + elemento2 + ' (' + (typeof elemento2) + ')' 
+
+}else if (elemento1 == elemento2){
+
+    mensagem = 'Elemento ' + elemento1 + ' (' + (typeof elemento1) + ')' + ' é equivalente ao elemento ' + elemento2 + ' (' + (typeof elemento2) + ')' 
+
+}else if (elemento1 != elemento2){
+    
+    mensagem = 'Elemento ' + elemento1 + ' (' + (typeof elemento1) + ')' + ' é diferente do elemento ' + elemento2 + ' (' + (typeof elemento2) + ')'  
+
+}
+
+return mensagem
+
+ }
